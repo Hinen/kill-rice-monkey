8652281d3fcf6dc5d10ec03bc35c81edf0cab706
Hinen <qkrqldtjqj@gmail.com>
2026-03-09 20:17:49 +0900

    chore: 미사용 Entity(TicketTarget) 및 NuGet 패키지 제거

--- src/KillRiceMonkey.Domain/Entities/TicketTarget.cs
+++
@@ -1,3 +0,0 @@
-namespace KillRiceMonkey.Domain.Entities;
-
-public sealed record TicketTarget(string EventKeyword, string TargetUrl);
--- src/KillRiceMonkey.Infrastructure/DependencyInjection.cs
+++ src/KillRiceMonkey.Infrastructure/DependencyInjection.cs
@@ -8,7 +8,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        services.AddHttpClient();
         services.AddSingleton<ITicketingAutomationService, PlaywrightTicketingAutomationService>();
         return services;
     }
